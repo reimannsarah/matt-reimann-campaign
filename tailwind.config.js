d5645d656431d2e1befa828deda0d5119c7267f7
Sarah Reimann <reimannsarah45@gmail.com>
2024-01-29 15:15:44 -0800

    add fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        header: ["Bebas Neue", "sans-serif"],
+        body: ["PT Sans Narroo", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 }
