c9e11a196213bbf1fefbbac54b520af96a11d2bf
Sarah Reimann <reimannsarah45@gmail.com>
2024-01-29 21:39:52 -0800

    Refactor Photo component layout

--- src/components/sub/Photo.tsx
+++ src/components/sub/Photo.tsx
@@ -2,7 +2,7 @@ import { imageUrls } from "../../assets/imgs/imageUrls"
 
 const Photo = () => {
   return (
-    <div className='absolute top-[800px] inset-x-0'>
+    <div >
       <img src={imageUrls.skyline} alt="" className='w-screen'/>
     </div>
   )
