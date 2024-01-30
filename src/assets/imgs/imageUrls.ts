5133dfd43c20aed5e5bf84e13b6e0fb9d884475a
Sarah Reimann <reimannsarah45@gmail.com>
2024-01-30 15:27:23 -0800

    Add new social media icons to imageUrls

--- src/assets/imgs/imageUrls.ts
+++ src/assets/imgs/imageUrls.ts
@@ -1,4 +1,7 @@
 export const imageUrls = {
   matt: 'https://mattreimanncampaign.blob.core.windows.net/photos/matt.png',
-  skyline:'https://mattreimanncampaign.blob.core.windows.net/photos/skyline.jpg'
+  skyline:'https://mattreimanncampaign.blob.core.windows.net/photos/skyline.jpg',
+  fb: 'https://mattreimanncampaign.blob.core.windows.net/photos/fb.png',
+  instagram: 'https://mattreimanncampaign.blob.core.windows.net/photos/instagram.png',
+  twitter: 'https://mattreimanncampaign.blob.core.windows.net/photos/twitter.png',
 }
