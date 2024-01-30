be177f10897b7326c687dec11146e05429e8f4af
Sarah Reimann <reimannsarah45@gmail.com>
2024-01-29 20:16:47 -0800

    Add NavBar component

---
+++ src/components/sub/NavBar.tsx
@@ -0,0 +1,25 @@
+import { NavLink } from 'react-router-dom'
+
+const NavBar = () => {
+  // const location = useLocation();
+  return (
+    <div className='relative'>
+      <div className='absolute top-44 right-0 flex flex-row gap-5 text-white font-body text-xl'>
+        <NavLink to="/home">
+          <p>Home</p>
+        </NavLink>
+        <NavLink to="/about">
+          <p>About</p>
+        </NavLink>
+        <NavLink to="/platform">
+          <p>Platform</p>
+        </NavLink>
+        <NavLink to="/endorsements">
+          <p>Endorsements</p>
+        </NavLink>
+      </div>
+    </div>
+  )
+}
+
+export default NavBar
