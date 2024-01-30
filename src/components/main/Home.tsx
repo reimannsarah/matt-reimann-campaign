89284621036abe6f77c03f6987f1fb460336480f
Sarah Reimann <reimannsarah45@gmail.com>
2024-01-29 20:16:24 -0800

    Add Home component

---
+++ src/components/main/Home.tsx
@@ -0,0 +1,13 @@
+import Hero from '../sub/Hero'
+import Photo from '../sub/Photo'
+
+const Home = () => {
+  return (
+    <div className='mt-80'>
+      <Hero />
+      <Photo />
+    </div>
+  )
+}
+
+export default Home
