f89becd41e4b294e9606180519a8b911d6c068fb
Sarah Reimann <reimannsarah45@gmail.com>
2024-01-30 15:27:47 -0800

    Update Footer component with new layout and content

--- src/components/sub/Footer.tsx
+++ src/components/sub/Footer.tsx
@@ -1,14 +1,45 @@
 import { NavLink } from "react-router-dom";
+import { appStrings } from "../../assets/text/appStrings";
+import { imageUrls } from "../../assets/imgs/imageUrls";
 
 const Footer = () => {
   return (
-    <footer className="absolute bottom-0">
-      <h1>Reimann for Portland District 4</h1>
-      <NavLink to="/about"></NavLink>
-      <NavLink to="/platform"></NavLink>
-      <NavLink to="/endorsements"></NavLink>
-      <NavLink to="/privacy"></NavLink>
-      <NavLink to="/donate"></NavLink>
+    <footer className="absolute top-[3000px] h-[300px] bg-white inset-x-0">
+      <div className="flex flex-row justify-between p-16">
+        <div className="text-right">
+          <h1
+            className="font-header text-4xl text-primary"
+            style={{ whiteSpace: "pre-line" }}
+          >
+            {appStrings.footer.header}
+          </h1>
+        </div>
+        <div className="flex flex-row gap-5">
+          <img src={imageUrls.fb} alt="facebook icon" className="w-10 h-10" />
+          <img
+            src={imageUrls.instagram}
+            alt="instagram icon"
+            className="w-10 h-10"
+          />
+          <img
+            src={imageUrls.twitter}
+            alt="twitter icon"
+            className="w-10 h-10"
+          />
+        </div>
+        <div className="flex flex-row gap-10">
+          <div className="flex flex-col font-body text-primary text-xl gap-5">
+            <NavLink to="/about">About</NavLink>
+            <NavLink to="/platform">Platform</NavLink>
+            <NavLink to="/contact">Contact</NavLink>
+          </div>
+          <div className="flex flex-col font-body text-primary text-xl gap-5">
+            <NavLink to="/endorsements">Endorsements</NavLink>
+            <NavLink to="/privacy">Privacy</NavLink>
+            <NavLink to="/donate">Donate</NavLink>
+          </div>
+        </div>
+      </div>
     </footer>
   );
 };
