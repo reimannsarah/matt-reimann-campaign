b17b38c79a300d32bb61e7910383455a8dd4b06b
Sarah Reimann <reimannsarah45@gmail.com>
2024-01-29 20:16:19 -0800

    Add appStrings module for donations

---
+++ src/assets/text/appStrings.ts
@@ -0,0 +1,11 @@
+export const appStrings = {
+  donations: {
+    header: 'Donations',
+    subheader1: '$20 donations matched 9 - 1',
+    info1: 'The City of Portland will match your first $20 with $180, turning your $20 into $200.',
+    subheader2: '$50 donations eligible for tax credit',
+    info2:'Claim Oregon&#39;s Political Contribution Tax Credit and get up to $50 back for a $50 donation',
+    button_copy: 'Donate Now',
+    learn_more: 'Learn More'
+  },
+}
