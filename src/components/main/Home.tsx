1a734ed015b5f832a71c62f49c142c347d4e5afc
Sarah Reimann <reimannsarah45@gmail.com>
2024-01-29 21:39:32 -0800

    Refactor Home component layout

--- src/components/main/Home.tsx
+++ src/components/main/Home.tsx
@@ -1,13 +1,21 @@
-import Hero from '../sub/Hero'
-import Photo from '../sub/Photo'
+import Hero from "../sub/Hero";
+import Photo from "../sub/Photo";
+import DonationsInfo from "../sub/DonationsInfo";
 
 const Home = () => {
   return (
-    <div className='mt-80'>
-      <Hero />
-      <Photo />
+    <div className="mt-80">
+      <div >
+        <Hero />
+      </div>
+      <div className='absolute top-[800px] inset-x-0'>
+        <Photo />
+      </div>
+      <div className="absolute top-[1100px] left-1/2 transform -translate-x-1/2">
+        <DonationsInfo />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
