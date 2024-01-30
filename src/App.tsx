1421d81c200e15f6612f4944592dfd29dc620bfe
Sarah Reimann <reimannsarah45@gmail.com>
2024-01-30 13:55:23 -0800

    Remove import statement for App.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Landing from './components/main/Landing'
 import Home from './components/main/Home'
