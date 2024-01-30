e215856380819f2748691dda8065cbd8dbe2cad3
Sarah Reimann <reimannsarah45@gmail.com>
2024-01-29 21:41:27 -0800

    Remove commented out code in DonationsInfo component

--- src/components/sub/DonationsInfo.tsx
+++ src/components/sub/DonationsInfo.tsx
@@ -6,7 +6,6 @@ const DonationsInfo = () => {
     <div className="p-10 mb-16">
       <div className="bg-white rounded-lg p-12">
         <div className="text-primary flex flex-col gap-3">
-          {/* <h1>{appStrings.donations.header}</h1> */}
           <h1 className="font-header text-3xl">
             {appStrings.donations.subheader1}
           </h1>
