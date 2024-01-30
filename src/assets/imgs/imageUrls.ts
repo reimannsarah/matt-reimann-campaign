39c579f58cb8f7180824b24e73812e51bd24816a
Sarah Reimann <reimannsarah45@gmail.com>
2024-01-29 20:16:15 -0800

    Add imageUrls.ts file with image URLs

---
+++ src/assets/imgs/imageUrls.ts
@@ -0,0 +1,4 @@
+export const imageUrls = {
+  matt: 'https://mattreimanncampaign.blob.core.windows.net/photos/matt.png',
+  skyline:'https://mattreimanncampaign.blob.core.windows.net/photos/skyline.jpg'
+}
