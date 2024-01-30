a57ebff3c767bcd4844d8c56a5bd31822b5241d0
Sarah Reimann <reimannsarah45@gmail.com>
2024-01-30 11:26:07 -0800

    Refactor Footer component to include navigation links

--- src/components/sub/Footer.tsx
+++ src/components/sub/Footer.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import { NavLink } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="absolute bottom-0">
+      <h1>Reimann for Portland District 4</h1>
+      <NavLink to="/about"></NavLink>
+      <NavLink to="/platform"></NavLink>
+      <NavLink to="/endorsements"></NavLink>
+      <NavLink to="/privacy"></NavLink>
+      <NavLink to="/donate"></NavLink>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
