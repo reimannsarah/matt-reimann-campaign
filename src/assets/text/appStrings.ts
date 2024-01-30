d7ff4dba83dc005deb7a170ea0d9033102ba82dd
Sarah Reimann <reimannsarah45@gmail.com>
2024-01-29 21:39:21 -0800

    Add hero button to appStrings.ts

--- src/assets/text/appStrings.ts
+++ src/assets/text/appStrings.ts
@@ -1,10 +1,13 @@
 export const appStrings = {
+  hero: {
+    button: 'Support the campaign',
+  },
   donations: {
     header: 'Donations',
     subheader1: '$20 donations matched 9 - 1',
     info1: 'The City of Portland will match your first $20 with $180, turning your $20 into $200.',
     subheader2: '$50 donations eligible for tax credit',
-    info2:'Claim Oregon&#39;s Political Contribution Tax Credit and get up to $50 back for a $50 donation',
+    info2:`Claim Oregon's Political Contribution Tax Credit and get up to $50 back for a $50 donation`,
     button_copy: 'Donate Now',
     learn_more: 'Learn More'
   },
