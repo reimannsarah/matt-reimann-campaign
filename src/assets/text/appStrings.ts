7861ff967d8fcd6fbbbcc1b8bfa63382dfd28a0a
Sarah Reimann <reimannsarah45@gmail.com>
2024-01-30 13:55:36 -0800

    Update appStrings.ts with new content

--- src/assets/text/appStrings.ts
+++ src/assets/text/appStrings.ts
@@ -1,15 +1,64 @@
 export const appStrings = {
   hero: {
-    slogan:'A fresh perspective from a lifelong Portlander',
-    button: 'Support the campaign',
+    slogan: "Bringing business\n and vitality back\n to Portland",
+    button: "Support the campaign",
   },
   donations: {
-    header: 'Donations',
-    subheader1: '$20 donations matched 9 - 1',
-    info1: 'The City of Portland will match your first $20 with $180, turning your $20 into $200.',
-    subheader2: '$50 donations eligible for tax credit',
-    info2:`Claim Oregon's Political Contribution Tax Credit and get up to $50 back for a $50 donation`,
-    button_copy: 'Donate Now',
-    learn_more: 'Learn More'
+    header: "Donations",
+    subheader1: "$20 donations matched 9 - 1",
+    info1:
+      "The City of Portland will match your first $20 with $180, turning your $20 into $200.",
+    subheader2: "$50 donations eligible for tax credit",
+    info2: `Claim Oregon's Political Contribution Tax Credit and get up to $50 back for a $50 donation`,
+    button_copy: "Donate Now",
+    learn_more: "Learn More",
   },
-}
+  platform: [
+    {
+      title: "Government Acountability",
+      subtitle:
+        "Establishing precedent for the City Commision - holding the city government accountable in areas of fiscal prudence and efficient delivery of services",
+      point1:
+        "Portland is a city of makers, artists, and entrepreneurs. We need to support and grow our local businesses and arts community.",
+      point2:
+        "With 12 commissioners focusing on policy and city governance, we will see measurable improvement in how we manage and deliver services to the citizens of Portland.  I intend to make sure that happens.",
+      point3:
+        "Establish precedence with Commision hearings to delve into important city issues - budget, emergency services, safety, streets and transportation, business support, and more..",
+      point4:
+        "Create clear and understandable metrics to measure our progress and improve the management and curation of public assets and funds",
+    },
+    {
+      title: "Houselessness",
+      subtitle:
+        "This is not a problem unique to Portland.  This is an issue that is nationwide.  Through surveys, and interviews, and studies (such as the PSU PiT reports), we know that this community is largely made up of people suffering from mental health, drug addiction, partner/family violenceand beingjust plain down on their luck,  (and some combination of all three).  We can fix this compassionately with an aim to break the cycle.  So it should be addressed from all angles - local, state and federal",
+      point1:
+        "I would advocate for a more coordinated and focused effort to help the houseless be housed, and get on a path to exiting the streets. I believe this is a crisis and this will take a huge focused effort across local, regional, and state agencies to solve",
+      point2:
+        "We are not short of money to address this, I don’t believe we need more.   Between Measure 10, Metro Supporting Housing Services tax, and Governor Kotek’s efforts we have the funding and the tools to tackle this problem",
+      point3:
+        "I would advocate for clear and impactful metrics to measure our progress",
+    },
+    {
+      title: "Culture and Commerce",
+      subtitle: "Open the city for business!",
+      point1:
+        "A growing and vibrant city brings new jobs, diversity, arts/entertainment, and much more",
+      point2:
+        "I would advocate to structure our tax, fee, and zoning code so that it supports our current big employers and encourages new and thriving industries (think Green businesses, AI learning, software apps) while not compromising but enhancing current delivery of city services and goals",
+      point3:
+        "Growing and supporting our businesses (new and current) means higher revenue to the city and better services to its constituents",
+      point4: "City buildings and offices need to be more accessible",
+      point5:
+        "I would push for a commission-study of the best practices in other large cities and seek ways to improve our messaging and attract more home-grown and incoming businesses",
+    },
+    {
+      title: "Community Outreach",
+      subtitle: `Improve our city's connection to its constituents.  With the new government structure each commissioner is elected by a district connecting them to a geographic area of Portland`,
+      point1: "Regular outreach with neighborhood associations",
+      point2:
+        "Address local issues that impact each community as well as individuals",
+      point3:
+        "Since the Commission offices are located in District 4, I would advocate that the Commission rotate its meeting location to each of the 4 districts throughout the year",
+    },
+  ],
+};
