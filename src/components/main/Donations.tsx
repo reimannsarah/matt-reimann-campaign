9581d1f83d5b0c182cce95cb3c384a35aeed8bf0
Sarah Reimann <reimannsarah45@gmail.com>
2024-01-30 13:55:40 -0800

    Add Donations component

---
+++ src/components/main/Donations.tsx
@@ -0,0 +1,12 @@
+import DonationsInfo from "../sub/DonationsInfo"
+
+const Donations = () => {
+  return (
+    <div className="mt-16">
+      <h1 className="font-header text-white text-8xl text-center">Donations</h1>
+      <DonationsInfo />
+    </div>
+  )
+}
+
+export default Donations
