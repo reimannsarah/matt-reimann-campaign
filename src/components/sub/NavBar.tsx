a86cfafdb236a6ef008d0197872dfb2734d9416e
Sarah Reimann <reimannsarah45@gmail.com>
2024-01-31 15:58:41 -0800

    Update NavBar component with motion and styling

--- src/components/sub/NavBar.tsx
+++ src/components/sub/NavBar.tsx
@@ -1,34 +1,45 @@
-import { NavLink, useLocation } from 'react-router-dom'
+import { NavLink, useLocation } from "react-router-dom";
+import { motion } from "framer-motion";
+import { slideInFromRight } from "../../../utils/motion";
 
 const NavBar = () => {
   const location = useLocation();
   return (
-    <div className='relative'>
-      <div className='absolute right-20 flex flex-row gap-5 text-white font-body text-xl'>
+    <motion.div
+      variants={slideInFromRight(0.7)}
+      initial="hidden"
+      animate="visible"
+      className="relative"
+    >
+      <div className="absolute right-20 flex flex-row gap-5 text-white font-body text-xl">
         <div></div>
-        <NavLink to="/home" className={
-          location.pathname === '/home' ? "underline" : ""
-        }>
-          <p className='hover:underline'>Home</p>
+        <NavLink
+          to="/home"
+          className={location.pathname === "/home" ? "underline" : ""}
+        >
+          <p className="hover:underline">Home</p>
         </NavLink>
-        <NavLink to="/about" className={
-          location.pathname === '/about' ? "underline" : ""
-        }>
-          <p className='hover:underline'>About</p>
+        <NavLink
+          to="/about"
+          className={location.pathname === "/about" ? "underline" : ""}
+        >
+          <p className="hover:underline">About</p>
         </NavLink>
-        <NavLink to="/platform" className={
-          location.pathname === '/platform' ? "underline" : ""
-        }>
-          <p className='hover:underline'>Platform</p>
+        <NavLink
+          to="/platform"
+          className={location.pathname === "/platform" ? "underline" : ""}
+        >
+          <p className="hover:underline">Platform</p>
         </NavLink>
-        <NavLink to="/endorsements" className={
-          location.pathname === '/endorsements' ? "underline" : ""
-        }>
-          <p className='hover:underline'>Endorsements</p>
+        <NavLink
+          to="/endorsements"
+          className={location.pathname === "/endorsements" ? "underline" : ""}
+        >
+          <p className="hover:underline">Endorsements</p>
         </NavLink>
       </div>
-    </div>
-  )
-}
+    </motion.div>
+  );
+};
 
-export default NavBar
+export default NavBar;
