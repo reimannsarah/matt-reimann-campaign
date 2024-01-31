067f108e9b3dd4eb932e1d88c58b67872e9f1f7c
Sarah Reimann <reimannsarah45@gmail.com>
2024-01-30 16:48:17 -0800

    Refactor Home component to use HomePlatform subcomponent

--- src/components/main/Home.tsx
+++ src/components/main/Home.tsx
@@ -1,23 +1,15 @@
 import Hero from "../sub/Hero";
 import Photo from "../sub/Photo";
 import Donations from "./Donations";
-import Platform from "./Platform";
+import HomePlatform from "../sub/HomePlatform";
 
 const Home = () => {
   return (
     <div className="mt-80">
-      <div >
         <Hero />
-      </div>
-      <div className='absolute top-[800px] inset-x-0'>
         <Photo />
-      </div>
-      <div className="absolute top-[1100px] left-1/2 transform -translate-x-1/2">
         <Donations />
-      </div>
-      <div className="absolute top-[1700px] left-1/2 transform -translate-x-1/2">
-        <Platform />
-      </div>
+        <HomePlatform />
     </div>
   );
 };
