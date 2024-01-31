927dd45d719e5353eb2e1ff92c62814bd547af72
Sarah Reimann <reimannsarah45@gmail.com>
2024-01-31 15:58:45 -0800

    Add motion and animation to Photo component

--- src/components/sub/Photo.tsx
+++ src/components/sub/Photo.tsx
@@ -1,11 +1,18 @@
 import { imageUrls } from "../../assets/imgs/imageUrls"
+import { motion } from "framer-motion"
+import { fadeIn } from "../../../utils/motion"
 
 const Photo = () => {
   return (
-    <div className="w-full">
+    <motion.div 
+    variants={fadeIn(0.5)}
+    initial="hidden"
+    animate="visible"
+    className="w-full"
+    >
       <img src={imageUrls.skyline} alt="" className='hidden md:block w-full object-cover'/>
       <img src={imageUrls.mobile_skyline} alt="" className="md:hidden w-full object-cover"/>
-    </div>
+    </motion.div>
   )
 }
 
