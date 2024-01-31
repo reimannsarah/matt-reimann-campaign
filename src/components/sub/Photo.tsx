b98b80c69f6b023f786ce104fb66ec968b0e13b8
Sarah Reimann <reimannsarah45@gmail.com>
2024-01-31 14:10:24 -0800

    Add responsive image display

--- src/components/sub/Photo.tsx
+++ src/components/sub/Photo.tsx
@@ -3,7 +3,8 @@ import { imageUrls } from "../../assets/imgs/imageUrls"
 const Photo = () => {
   return (
     <div className="w-full">
-      <img src={imageUrls.skyline} alt="" className='w-full object-cover'/>
+      <img src={imageUrls.skyline} alt="" className='hidden md:block w-full object-cover'/>
+      <img src={imageUrls.mobile_skyline} alt="" className="md:hidden w-full object-cover"/>
     </div>
   )
 }
