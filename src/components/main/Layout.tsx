f09dbf6b9fbac7f8e0fcf53b711d1902e0016886
Sarah Reimann <reimannsarah45@gmail.com>
2024-01-30 16:48:21 -0800

    Refactor layout component

--- src/components/main/Layout.tsx
+++ src/components/main/Layout.tsx
@@ -1,13 +1,11 @@
 import { Outlet } from "react-router-dom";
-import Navbar from "../sub/NavBar";
 import Footer from "../sub/Footer";
-import Header from "../sub/Header";
+import Header from "../main/Header";
 
 const Layout = () => {
   return (
     <div className="flex flex-col justify-between">
       <Header />
-      <Navbar />
       <div className="flex-grow">
         <Outlet />
       </div>
