ae2721f1e1652c4797e7cecb3c3360c736075333
Sarah Reimann <reimannsarah45@gmail.com>
2024-01-31 14:50:50 -0800

    Add responsive screen sizes to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    screens: {
+      sm: "640px",
+      mini: "500px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
     extend: {
       fontFamily: {
         header: ["Bebas Neue", "sans-serif"],
