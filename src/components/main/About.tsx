6b7e08ca8566cf7827eb2efee7f4a9dd0a1bbb8f
Sarah Reimann <reimannsarah45@gmail.com>
2024-01-31 15:30:29 -0800

    Refactor About component to include dynamic content

--- src/components/main/About.tsx
+++ src/components/main/About.tsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import { appStrings } from "../../assets/text/appStrings";
+import { imageUrls } from "../../assets/imgs/imageUrls";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div>
+      <img src={imageUrls.mobile_skyline} className="md:hidden" alt="" />
+      <div className="bg-white p-8 mb-16 md:rounded-lg md:mx-36 md:mt-16">
+        <div className="flex flex-col gap-4">
+          <h1 className="font-header text-primary text-4xl md:text-6xl">
+            {appStrings.about.title}
+          </h1>
+          <p className="font-body text-secondary text-sm md:text-lg">
+            {appStrings.about.p1}
+          </p>
+          <p className="font-body text-secondary text-sm md:text-lg">
+            {appStrings.about.p2}
+          </p>
+          <p className="font-body text-secondary text-sm md:text-lg">
+            {appStrings.about.p3}
+          </p>
+          <p className="font-body text-secondary text-sm md:text-lg">
+            {appStrings.about.p4}
+          </p>
+          <p className="font-body text-secondary text-sm md:text-lg">
+            {appStrings.about.p5}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default About
+export default About;
