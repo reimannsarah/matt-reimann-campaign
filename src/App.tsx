c32daf454b55ab0d48f3c94087ead0cc3d780cc7
Sarah Reimann <reimannsarah45@gmail.com>
2024-01-31 15:58:24 -0800

    Commented out Landing component import and removed Landing route

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Landing from "./components/main/Landing";
+// import Landing from "./components/main/Landing";
 import Home from "./components/main/Home";
 import Layout from "./components/main/Layout";
 import About from "./components/main/About";
@@ -10,7 +10,6 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Landing />} />
         <Route path="/*" element={<Layout />}>
           <Route path="home" element={<Home />} />
           <Route path="about" element={<About />} />
