cbaca1cd33145cc9e2870074e42797afbb9fa867
Sarah Reimann <reimannsarah45@gmail.com>
2024-01-30 16:48:00 -0800

    Refactor routing in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Landing from './components/main/Landing'
-import Home from './components/main/Home'
-import Layout from './components/main/Layout'
-import About from './components/main/About'
-import Platform from './components/main/Platform'
-import Endorsements from './components/main/Endorsements'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Landing from "./components/main/Landing";
+import Home from "./components/main/Home";
+import Layout from "./components/main/Layout";
+import About from "./components/main/About";
+import Platform from "./components/main/Platform";
+import Endorsements from "./components/main/Endorsements";
 
 function App() {
-
   return (
     <Router>
       <Routes>
-            <Route path="/" element={<Landing />} />
-            <Route path="/*" element={<Layout />}>
-              <Route path="home" element={<Home />} />
-              <Route path="about" element={<About />} />
-              <Route path="platform" element={<Platform />} />
-              <Route path="endorsements" element={<Endorsements />} />
-            </Route>
-          </Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/*" element={<Layout />}>
+          <Route path="home" element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="platform" element={<Platform />} />
+          <Route path="endorsements" element={<Endorsements />} />
+        </Route>
+      </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
