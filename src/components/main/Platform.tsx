0ffe48ddc6a42149f8e25a4d0937f70b55f6d985
Sarah Reimann <reimannsarah45@gmail.com>
2024-01-31 15:18:20 -0800

    style for breakpoints

--- src/components/main/Platform.tsx
+++ src/components/main/Platform.tsx
@@ -1,30 +1,44 @@
-import { appStrings } from "../../assets/text/appStrings"
+import { appStrings } from "../../assets/text/appStrings";
+import { imageUrls } from "../../assets/imgs/imageUrls";
 
 const Platform = () => {
   return (
-    <div className="mt-[350px] mx-36 bg-white p-14 rounded-lg">
-      <h1>Title</h1>
-      <p>{appStrings.platform_intro.p1}</p>
-      <p>{appStrings.platform_intro.p2}</p>
-      <p>{appStrings.platform_intro.p3}</p>
-      <div>
-        {appStrings.platform.map((item, index) => {
-          return (
-            <div key={index} className="">
-              <h1>{item.title}</h1>
-              <ul>
-                <li>{item.point1}</li>
-                <li>{item.point2}</li>
-                <li>{item.point3}</li>
-                {item.point4 && <li>{item.point4}</li>}
-                {item.point5 && <li>{item.point5}</li>}
-              </ul>
-            </div>
-          )
-        })}
+    <div>
+      <img src={imageUrls.mobile_skyline} className="md:hidden" alt="" />
+      <div className="p-10 mb-24 md:mt-36 md:mx-36 bg-white md:p-14 md:rounded-lg">
+        <h1 className="font-header text-primary text-4xl md:text-6xl mb-5">{appStrings.platform_intro.title}</h1>
+        <div className="text-sm flex flex-col gap-4 text-secondary md:text-lg">
+          <strong>
+            <p>{appStrings.platform_intro.p1}</p>
+          </strong>
+          <strong>
+            <p>{appStrings.platform_intro.p2}</p>
+          </strong>
+          <strong>
+            <p>{appStrings.platform_intro.p3}</p>
+          </strong>
+        </div>
+        <div>
+          {appStrings.platform.map((item, index) => {
+            return (
+              <div key={index} className="mt-10">
+                <h1 className="font-header text-primary text-2xl mb-4 md:text-4xl">
+                  {item.title}
+                </h1>
+                <ul className="flex flex-col gap-4 text-secondary md:text-lg">
+                  <li>{item.point1}</li>
+                  <li>{item.point2}</li>
+                  <li>{item.point3}</li>
+                  {item.point4 && <li>{item.point4}</li>}
+                  {item.point5 && <li>{item.point5}</li>}
+                </ul>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Platform
+export default Platform;
