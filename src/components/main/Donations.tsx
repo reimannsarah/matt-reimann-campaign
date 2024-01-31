6e70e62e9f6cf0d9efb54335e69b0580119881c9
Sarah Reimann <reimannsarah45@gmail.com>
2024-01-31 14:51:01 -0800

    Update Donations component styling

--- src/components/main/Donations.tsx
+++ src/components/main/Donations.tsx
@@ -2,8 +2,8 @@ import DonationsInfo from "../sub/DonationsInfo"
 
 const Donations = () => {
   return (
-    <div className="mt-16">
-      <h1 className="font-header text-white mb-8 md:mb-0 text-4xl md:text-6xl text-center">Donations</h1>
+    <div className="mt-16 mini:mx-80">
+      <h1 className="font-header text-white mb-8 md:mb-0 text-4xl md:text-8xl text-center">Donations</h1>
       <DonationsInfo />
     </div>
   )
