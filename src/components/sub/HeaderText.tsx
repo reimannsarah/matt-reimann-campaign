657a7e85d13f10e43dc2d5e556402eb0627c7504
Sarah Reimann <reimannsarah45@gmail.com>
2024-01-30 16:49:13 -0800

    rename and move file

--- src/components/sub/HeaderText.tsx
+++ src/components/sub/HeaderText.tsx
@@ -2,7 +2,7 @@
 const Header = () => {
   return (
     <div className="relative">
-      <div className="absolute top-24 left-0 text-white flex flex-col gap-3">
+      <div className="text-center md:top-20 md:left-20 text-white flex flex-col gap-3">
       <h1 className="font-header text-8xl">Matt Reimann</h1>
       <p className="font-header text-4xl text-center">For Portland City Council District 4</p>
       </div>
