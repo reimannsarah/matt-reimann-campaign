69e7154e1843f17f5bdb8d6506723680bace3198
Sarah Reimann <reimannsarah45@gmail.com>
2024-01-31 14:08:32 -0800

    add photo for mobile

--- src/assets/imgs/imageUrls.ts
+++ src/assets/imgs/imageUrls.ts
@@ -1,6 +1,7 @@
 export const imageUrls = {
   matt: 'https://mattreimanncampaign.blob.core.windows.net/photos/matt.png',
   skyline:'https://mattreimanncampaign.blob.core.windows.net/photos/skyline.jpg',
+  mobile_skyline: 'https://mattreimanncampaign.blob.core.windows.net/photos/mobile_skyline.jpg',
   fb: 'https://mattreimanncampaign.blob.core.windows.net/photos/fb.png',
   instagram: 'https://mattreimanncampaign.blob.core.windows.net/photos/instagram.png',
   twitter: 'https://mattreimanncampaign.blob.core.windows.net/photos/twitter.png',
