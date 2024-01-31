ad8cfb7518dc291d3e472fc085099d0113a161a5
Sarah Reimann <reimannsarah45@gmail.com>
2024-01-31 14:08:58 -0800

    add styling for mobile

--- src/components/main/Donations.tsx
+++ src/components/main/Donations.tsx
@@ -3,7 +3,7 @@ import DonationsInfo from "../sub/DonationsInfo"
 const Donations = () => {
   return (
     <div className="mt-16">
-      <h1 className="font-header text-white mb-8 md:mb-0 text-6xl text-center">Donations</h1>
+      <h1 className="font-header text-white mb-8 md:mb-0 text-4xl md:text-6xl text-center">Donations</h1>
       <DonationsInfo />
     </div>
   )
