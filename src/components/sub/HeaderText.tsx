badabd9aed0c24f5731bcfe9ae32f9271377ecc1
Sarah Reimann <reimannsarah45@gmail.com>
2024-01-31 15:58:36 -0800

    Add motion animation to HeaderText component

--- src/components/sub/HeaderText.tsx
+++ src/components/sub/HeaderText.tsx
@@ -1,12 +1,19 @@
+import { motion } from "framer-motion";
+import { slideInFromLeft } from "../../../utils/motion";
 
 const Header = () => {
   return (
-    <div className="relative">
+    <motion.div 
+    variants={slideInFromLeft(0.5)}
+    initial="hidden"
+    animate="visible"
+    className="relative"
+    >
       <div className="text-center md:top-20 md:left-20 text-white flex flex-col gap-3">
       <h1 className="font-header text-8xl">Matt Reimann</h1>
       <p className="font-header text-4xl text-center">For Portland City Council District 4</p>
       </div>
-    </div>
+    </motion.div>
   )
 }
 
