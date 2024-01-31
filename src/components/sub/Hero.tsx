daf67571e0bb56747f179f658f0267a2d88bc21a
Sarah Reimann <reimannsarah45@gmail.com>
2024-01-31 14:51:13 -0800

    Refactor Hero component layout and styles

--- src/components/sub/Hero.tsx
+++ src/components/sub/Hero.tsx
@@ -3,28 +3,43 @@ import { appStrings } from "../../assets/text/appStrings";
 
 const Hero = () => {
   return (
-      <div className="flex flex-row justify-center">
-        <div className="flex flex-col mt-14">
-          <p
-            className="font-subheader text-white text-3xl text-right"
-            style={{ whiteSpace: "pre-line" }}
-          >
-            {
-              appStrings.hero.slogan
-            }
-          </p>
-          <div className="flex justify-end mt-16">
-            <button className="bg-white rounded-lg text-primary py-3 px-5 font-body">{appStrings.hero.button}</button>
-          </div>
-        </div>
-        <img
-          src={imageUrls.matt}
-          alt=""
-          id="hero-img"
-          className="object-cover float-right"
-        />
+    <div className="md:flex md:text-center md:items-center md:justify-center">
+      <div className="inline">
+        <p
+          className="font-subheader md:hidden text-white text-2xl ml-3 mini:ml-12 mini:text-4xl"
+          style={{ whiteSpace: "pre-line" }}
+        >
+          {appStrings.hero.slogan}
+        </p>
+        <p
+          className="hidden md:block font-subheader text-white ml-3 text-6xl"
+          style={{ whiteSpace: "pre-line" }}
+        >
+          {appStrings.hero.slogan_lg}
+        </p>
+        <button className="bg-white hidden border-2 border-white mini:block mini:mt-24 md:mx-auto rounded-lg ml-3 text-primary py-3 px-5 font-body hover:bg-primary hover:text-white">
+          {appStrings.hero.button_long}
+        </button>
       </div>
+      <img
+        src={imageUrls.matt}
+        alt="photo of Matt Reimann"
+        id="matt_img"
+        className="mr-0 w-3/5 md:w-1/5 object-cover float-right"
+      />
+    </div>
   );
 };
 
 export default Hero;
+
+{
+  /* <div className="flex justify-end mt-8 md:mt-16">
+          <button className="bg-white rounded-lg text-primary text-sm py-2 px-3 md:py-3 md:px-5 font-body">
+            {appStrings.hero.button}
+          </button>
+        </div> */
+}
+{
+  /* </div> */
+}
