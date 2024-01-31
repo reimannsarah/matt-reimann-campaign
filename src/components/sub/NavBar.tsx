6757c69b649b3739722a177faf5d9fbc6054c188
Sarah Reimann <reimannsarah45@gmail.com>
2024-01-31 14:58:40 -0800

    Add dynamic underline to active NavLink components

--- src/components/sub/NavBar.tsx
+++ src/components/sub/NavBar.tsx
@@ -1,22 +1,30 @@
-import { NavLink } from 'react-router-dom'
+import { NavLink, useLocation } from 'react-router-dom'
 
 const NavBar = () => {
-  // const location = useLocation();
+  const location = useLocation();
   return (
     <div className='relative'>
-      <div className='absolute top-44 right-20 flex flex-row gap-5 text-white font-body text-xl'>
+      <div className='absolute right-20 flex flex-row gap-5 text-white font-body text-xl'>
         <div></div>
-        <NavLink to="/home">
-          <p>Home</p>
+        <NavLink to="/home" className={
+          location.pathname === '/home' ? "underline" : ""
+        }>
+          <p className='hover:underline'>Home</p>
         </NavLink>
-        <NavLink to="/about">
-          <p>About</p>
+        <NavLink to="/about" className={
+          location.pathname === '/about' ? "underline" : ""
+        }>
+          <p className='hover:underline'>About</p>
         </NavLink>
-        <NavLink to="/platform">
-          <p>Platform</p>
+        <NavLink to="/platform" className={
+          location.pathname === '/platform' ? "underline" : ""
+        }>
+          <p className='hover:underline'>Platform</p>
         </NavLink>
-        <NavLink to="/endorsements">
-          <p>Endorsements</p>
+        <NavLink to="/endorsements" className={
+          location.pathname === '/endorsements' ? "underline" : ""
+        }>
+          <p className='hover:underline'>Endorsements</p>
         </NavLink>
       </div>
     </div>
