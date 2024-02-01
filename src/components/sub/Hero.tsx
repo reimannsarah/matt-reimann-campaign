6c58420ff1af670eeabbe42b150be6a9ec830a7b
Sarah Reimann <reimannsarah45@gmail.com>
2024-01-31 16:39:28 -0800

    Refactor Hero component layout and styles

--- src/components/sub/Hero.tsx
+++ src/components/sub/Hero.tsx
@@ -5,35 +5,37 @@ import { fadeIn } from "../../../utils/motion";
 
 const Hero = () => {
   return (
-    <motion.div 
-    variants={fadeIn(0.5)}
-    initial="hidden"
-    animate="visible"
-    className="md:flex md:text-center md:items-center md:justify-center"
+    <motion.div
+      variants={fadeIn(0.5)}
+      initial="hidden"
+      animate="visible"
+      className="md:flex md:text-center md:items-center md:justify-center"
     >
-      <div className="inline">
-        <p
-          className="font-subheader md:hidden text-white text-2xl ml-3 mini:ml-12 mini:text-4xl"
-          style={{ whiteSpace: "pre-line" }}
-        >
-          {appStrings.hero.slogan}
-        </p>
-        <p
-          className="hidden md:block font-subheader text-white ml-3 text-6xl"
-          style={{ whiteSpace: "pre-line" }}
-        >
-          {appStrings.hero.slogan_lg}
-        </p>
-        <button className="bg-white hidden border-2 border-white mini:block mini:mt-24 md:mx-auto rounded-lg ml-3 text-primary py-3 px-5 font-body hover:bg-primary hover:text-white">
-          {appStrings.hero.button_long}
-        </button>
+      <div className="flex">
+        <div className="inline">
+          <p
+            className="font-subheader w-full text-white text-2xl ml-7 pb-3 mini:ml-12 mini:text-4xl md:hidden"
+            style={{ whiteSpace: "pre-line" }}
+          >
+            {appStrings.hero.slogan}
+          </p>
+          <p
+            className="hidden md:block font-subheader text-white ml-3 text-6xl"
+            style={{ whiteSpace: "pre-line" }}
+          >
+            {appStrings.hero.slogan_lg}
+          </p>
+          <button className="bg-white border-2 border-white mini:block mini:mt-24 md:mx-auto rounded-lg ml-3 text-primary py-3 px-5 font-body hover:bg-primary hover:text-white">
+            {appStrings.hero.button_long}
+          </button>
+        </div>
+        <img
+          src={imageUrls.matt}
+          alt="photo of Matt Reimann"
+          id="matt_img"
+          className="mr-0 w-3/5 md:w-1/5 object-cover float-right"
+        />
       </div>
-      <img
-        src={imageUrls.matt}
-        alt="photo of Matt Reimann"
-        id="matt_img"
-        className="mr-0 w-3/5 md:w-1/5 object-cover float-right"
-      />
     </motion.div>
   );
 };
