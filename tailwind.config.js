29a4201ef3cb980c33d944f2bc1378f7b2b36669
Sarah Reimann <reimannsarah45@gmail.com>
2024-02-26 14:51:30 -0800

    Update tailwind.config.js with new content and boxShadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     screens: {
       sm: "640px",
@@ -12,19 +9,21 @@ export default {
       lg: "1024px",
       xl: "1280px",
     },
+    boxShadow: {
+      inner: "inset 0 2px 4px 2px rgba(0, 0, 0, 0.2)",
+    },
     extend: {
       fontFamily: {
         header: ["Bebas Neue", "sans-serif"],
         body: ["PT Sans Narroo", "sans-serif"],
       },
       colors: {
-        background: '#0C2229',
-        green: '#C0DE00',
-        indigo: '#2E3FFF',
-        orange: '#FF5E00',
-      }
+        background: "#0C2229",
+        green: "#C0DE00",
+        indigo: "#2E3FFF",
+        orange: "#FF5E00",
+      },
     },
   },
   plugins: [],
-}
-
+};
