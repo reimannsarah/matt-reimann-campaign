1f99564849eb083cdf0216932b2b5703f7a8f643
Sarah Reimann <reimannsarah45@gmail.com>
2024-02-26 14:04:15 -0800

    add new photo

--- src/assets/imgs/imageUrls.ts
+++ src/assets/imgs/imageUrls.ts
@@ -1,5 +1,6 @@
 export const imageUrls = {
   matt: 'https://mattreimanncampaign.blob.core.windows.net/photos/matt.png',
+  matt2: 'https://mattreimanncampaign.blob.core.windows.net/photos/matt2.jpeg',
   skyline:'https://mattreimanncampaign.blob.core.windows.net/photos/skyline.jpg',
   mobile_skyline: 'https://mattreimanncampaign.blob.core.windows.net/photos/mobile_skyline.jpg',
   fb: 'https://mattreimanncampaign.blob.core.windows.net/photos/fb.png',
